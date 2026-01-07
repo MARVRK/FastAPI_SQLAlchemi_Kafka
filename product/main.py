5689e81e560862f7bd0a8fc411c044b694923b66
MARVRK <155836501+MARVRK@users.noreply.github.com>
2026-01-07 21:54:36 +0100

    rebuild and split 3 separate uv + dockerfile + .venv

--- product/main.py
+++ product/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.product.api.product import router as product_router
+from api.product import router as product_router
 
 app = FastAPI()
 app.include_router(router=product_router)
--- user/main.py
+++ user/main.py
@@ -1,4 +1,4 @@
 from fastapi import FastAPI
-from app.user.api.user import router as user_router
+from api.user import router as user_router
 app = FastAPI()
 app.include_router(router=user_router)
