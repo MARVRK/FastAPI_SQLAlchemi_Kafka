e98e4aaea0793e47100974ebdcb4f5b2f8e8060b
MARVRK <155836501+MARVRK@users.noreply.github.com>
2026-01-29 22:22:51 +0100

    create settings for user/order/product

---
+++ user/infra/settings.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+class Settings(BaseSettings):
+	model_config = SettingsConfigDict(env_file=BASE_DIR / ".env",
+	                                  env_file_encoding="utf-8",
+									  extra = "ignore")
+	POSTGRES_DB_USERS : str
+	POSTGRES_USER_USERS : str
+	POSTGRES_PASSWORD_USERS :str
+	PG_CONNECTION_USERS :str
+
+settings = Settings()
