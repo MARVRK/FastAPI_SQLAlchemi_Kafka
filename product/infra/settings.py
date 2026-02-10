03cc75556a299751e96435d9e1eb63368250d43d
MARVRK <155836501+MARVRK@users.noreply.github.com>
2026-02-10 22:58:40 +0100

    remove env files

--- product/infra/settings.py
+++ product/infra/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 class Settings(BaseSettings):
-	model_config = SettingsConfigDict(env_file=BASE_DIR / ".env",
+	model_config = SettingsConfigDict(env_file=BASE_DIR / ".env.local",
 	                                  env_file_encoding="utf-8",
 									  extra = "ignore")
 	POSTGRES_DB_PRODUCTS : str
