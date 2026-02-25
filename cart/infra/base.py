91f4d226bd0930d5fdb2a316429160d306253289
MARVRK <155836501+MARVRK@users.noreply.github.com>
2026-02-26 00:52:02 +0100

    Updated project clean version

--- cart/infra/base.py
+++ cart/infra/base.py
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
+=======
+from contextlib import asynccontextmanager
+>>>>>>> 2a0205b (Updated project clean version)
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from cart.infra.settings import settings
 
+<<<<<<< HEAD
 engine = create_async_engine(url=settings.PG_CONNECTION_CART)
 SessionLocal = async_sessionmaker(bind=engine)
 
@@ -13,3 +18,31 @@ def get_db():
 		db.close()
 class Base(DeclarativeBase):
 	pass
+=======
+
+class Base(DeclarativeBase):
+	pass
+class DBSessionConfig:
+	def __init__(self, db_url: str):
+		self._engine = create_async_engine(url=db_url)
+		self._sessionmaker = async_sessionmaker(bind=self._engine, autoflush=False, expire_on_commit=True)
+	@asynccontextmanager
+	async def session(self):
+		if self._engine is None:
+			raise Exception("DBSessionConfig is not established!")
+
+		session = self._sessionmaker()
+		try:
+			yield session
+		except Exception as e:
+			await session.rollback()
+			raise e
+		finally:
+			await session.close()
+
+session_manager = DBSessionConfig(db_url=settings.PG_CONNECTION_CART)
+
+async def get_db():
+	async with session_manager.session() as session:
+		yield session
+>>>>>>> 2a0205b (Updated project clean version)
