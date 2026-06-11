0a281c3563fab2f3b71ac921999adad1490091a8
MARVRK <155836501+MARVRK@users.noreply.github.com>
2026-06-11 18:53:24 +0200

    feat(product): add ProductError

---
+++ product/error/error.py
@@ -0,0 +1,10 @@
+class ProductError(Exception):
+    def __init__(self, message, original_exception=None):
+        self.message = message
+        self.original_exception = original_exception
+        super().__init__(message)
+
+    def __str__(self):
+        if self.original_exception:
+            return f"(DB_PRODUCT_ERROR: {self.message}) caused by {self.original_exception}"
+        return f"(DB_PRODUCT_ERROR: {self.message})"
