83bd7c5fb0ff684c12af11f3ad19980f8b28d9d7
MARVRK <155836501+MARVRK@users.noreply.github.com>
2026-06-11 20:29:46 +0200

    feat(user): add User and RefreshToken models

--- user/models/user.py
+++ user/models/user.py
@@ -1,25 +1,30 @@
-from sqlalchemy import Integer, String, Boolean, DateTime
-from user.infra.base import Base
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import func, ForeignKey
 import datetime
+
+from sqlalchemy import Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import func
+
+from user.infra.base import Base
 
 
 class User(Base):
-	__tablename__ = "user"
+    __tablename__ = "user"
 
-	id: Mapped[int] = mapped_column(Integer, primary_key=True)
-	email: Mapped[str] = mapped_column(String(length=50), unique=True, nullable=False)
-	encrypted_password: Mapped[str] = mapped_column(String(length=200))
-	created_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-	updated_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now())
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(length=50), unique=True, nullable=False)
+    encrypted_password: Mapped[str] = mapped_column(String(length=200))
+    role: Mapped[str] = mapped_column(String(length=20), nullable=False, default="user")
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
+
 
 class RefreshToken(Base):
-	__tablename__ = "refresh_token"
+    __tablename__ = "refresh_token"
 
-	id: Mapped[int] = mapped_column(Integer, primary_key=True)
-	user_id: Mapped[int] = ForeignKey("user.id"), mapped_column(Integer, nullable=False)
-	refresh_token: Mapped[str] = mapped_column(String)
-	created_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
-	expires_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True))
-	revoked: Mapped[bool] = mapped_column(Boolean, default=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
+    refresh_token: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    expires_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True))
+    revoked: Mapped[bool] = mapped_column(Boolean, default=False)
