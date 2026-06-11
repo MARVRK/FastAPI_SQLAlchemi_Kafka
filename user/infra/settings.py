20ed7119c1135f0b6174d7970c78f3649e8e897e
MARVRK <155836501+MARVRK@users.noreply.github.com>
2026-06-11 20:31:54 +0200

    remove local varialbes from attributs

--- user/infra/settings.py
+++ user/infra/settings.py
@@ -1,11 +1,23 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
 class Settings(BaseSettings):
-	model_config = SettingsConfigDict(env_file=BASE_DIR / ".env.docker",
-	                                  env_file_encoding="utf-8",
-									  extra = "ignore")
-	PG_CONNECTION_USERS :str
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env.docker",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+    PG_CONNECTION_USERS: str
+
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int
+    JWT_REFRESH_TOKEN_EXPIRE_DAYS: int
+
 
 settings = Settings()
